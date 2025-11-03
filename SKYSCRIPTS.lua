c2e812e25799a37c05e44fbadc9e6f214b5be110
DEVSKY <131588969+boregondev@users.noreply.github.com>
2025-11-03 09:23:54 +0100

    Update

--- SKYSCRIPTS.lua
+++ SKYSCRIPTS.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.authguard.org/virtual-file/504c7ee13a914293ab202c240bd5eb80"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f1bbb653fe09f5421d52a1e54a407a95.lua"))()
